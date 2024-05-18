45b62ec0bcd80e362188e0d564363edd45f07e00
Apoliak7777 <167175652+Apoliak7777@users.noreply.github.com>
2024-05-18 08:49:02 +0200

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section id="home" className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-5xl font-bold">Welcome to My Portfolio</h1>
+    </section>
+  );
+}
